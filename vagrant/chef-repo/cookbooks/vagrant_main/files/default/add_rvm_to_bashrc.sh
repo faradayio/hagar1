adcff74d5c153c1aab5dd0de53aef53f4cb0afed
Seamus Abshere <seamus@abshere.net>
2010-10-07 08:43:08 -0500

    fix

--- vagrant/chef-repo/cookbooks/vagrant_main/files/default/add_rvm_to_bashrc.sh
+++ vagrant/chef-repo/cookbooks/vagrant_main/files/default/add_rvm_to_bashrc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-if grep --silent '[[ -s "/usr/local/lib/rvm" ]]' .bashrc
-	then
-	echo "rvm function already added."
-else
-	sed '$ a [[ -s "/usr/local/lib/rvm" ]] && . "/usr/local/lib/rvm"' --in-place="" .bashrc
-fi
-
-if grep --silent 'PS1="`/usr/bin/ruby_version.rb` $PS1"' .bashrc
+if grep --silent 'ruby_version.rb' $1
 	then
 	echo "rvm prompt already added."
 else
-	sed '/[[ -s "/usr/local/lib/rvm" ]]/ i PS1="`/usr/bin/ruby_version.rb` $PS1"' --in-place="" .bashrc
+	sed '$ a PS1="\`/usr/bin/ruby_version.rb\` $PS1"' --in-place="" $1
+fi
+
+if grep --silent '/usr/local/lib/rvm' $1
+	then
+	echo "rvm function already added."
+else
+	sed '$ a [[ -s "/usr/local/lib/rvm" ]] && . "/usr/local/lib/rvm"' --in-place="" $1
 fi
