402c4e0cde7e75dc64087b269e9d5639a0df2d16
Seamus Abshere <seamus@abshere.net>
2010-10-07 10:34:28 -0500

    switch around order of what goes in bashrc

--- vagrant/chef-repo/cookbooks/0_vagrant_main/files/default/add_rvm_to_bashrc.sh
+++ vagrant/chef-repo/cookbooks/0_vagrant_main/files/default/add_rvm_to_bashrc.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-if grep --silent 'ruby_version.rb' $1
-	then
-	echo "rvm prompt already added."
-else
-	sed '$ a PS1="\`/usr/bin/ruby_version.rb\` $PS1"' --in-place="" $1
-fi
-
 if grep --silent '/usr/local/lib/rvm' $1
 	then
 	echo "rvm function already added."
 else
 	sed '$ a [[ -s "/usr/local/lib/rvm" ]] && . "/usr/local/lib/rvm"' --in-place="" $1
+fi
+
+# put this second so it picks up on the default version
+if grep --silent 'ruby_version.rb' $1
+	then
+	echo "rvm prompt already added."
+else
+	sed '$ a PS1="\`/usr/bin/ruby_version.rb\` $PS1"' --in-place="" $1
 fi
